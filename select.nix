d4a7061e49d49debd00282120f083b72ee850492
lassulus <git@lassul.us>
2025-03-25 16:28:00 -0700

    rebuild select with proper parsing

--- select.nix
+++ select.nix
@@ -1,112 +1,256 @@
 rec {
-  recursiveSelect =
-    selectorIndex: selectorList: target:
+  /**
+      parseSelector :: String -> [ Selector ]
+
+      Example:
+      parseSelector ''*.{foo,bla}.123.hello''
+      => [
+        { type = "all"; }
+        { type = "set" values = [ "foo" "bla" ]; }
+        { type = "str" value = "123"; }
+        { type = "str" value = "hello"; }
+      ]
+  */
+  parseSelector =
+    x:
     let
-      selector = builtins.elemAt selectorList selectorIndex;
-    in
-
-    # selector is empty, we are done
-    if selectorIndex + 1 > builtins.length selectorList then
-      target
-
-    else if builtins.isList target then
-      # support bla.* for lists and recurse into all elements
-      if selector == "*" then
-        builtins.map (v: recursiveSelect (selectorIndex + 1) selectorList v) target
-      # support bla.3 for lists and recurse into the 4th element
-      else if (builtins.match "[[:digit:]]*" selector) == [ ] then
-        recursiveSelect (selectorIndex + 1) selectorList (
-          builtins.elemAt target (builtins.fromJSON selector)
-        )
-      # support bla.{1,3} for lists and recurse into the second and fourth elements
-      else if (builtins.match ''^\{([^}]*)}$'' selector) != null then
+      recurse =
+        str: idx: state:
         let
-          elementsToGet = map builtins.fromJSON (
-            builtins.filter (x: !builtins.isList x) (
-              builtins.split "," (builtins.head (builtins.match ''^\{([^}]*)}$'' selector))
-            )
-          );
-        in map (i: recursiveSelect (selectorIndex + 1) selectorList (builtins.elemAt target i)) elementsToGet
-      else
-        throw "only *, {n,n} or a number is allowed in list selector"
-
-    else if builtins.isAttrs target then
-      # handle the case bla.x.*.z where x is an attrset and we recurse into all elements
-      if selector == "*" then
-        builtins.mapAttrs (_: v: recursiveSelect (selectorIndex + 1) selectorList v) target
-      # support bla.{x,y,z}.world where we get world from each of x, y and z
-      else if (builtins.match ''^\{([^}]*)}$'' selector) != null then
-        let
-          attrsAsList = (
-            builtins.filter (x: !builtins.isList x) (
-              builtins.split "," (builtins.head (builtins.match ''^\{([^}]*)}$'' selector))
-            )
-          );
-          dummyAttrSet = builtins.listToAttrs (
-            map (x: {
-              name = x;
-              value = null;
-            }) attrsAsList
-          );
-          filteredAttrs = builtins.intersectAttrs dummyAttrSet target;
+          cur = builtins.substring idx 1 str;
+          mode = if state.stack == [ ] then "start" else builtins.head state.stack;
         in
-        builtins.mapAttrs (_: v: recursiveSelect (selectorIndex + 1) selectorList v) filteredAttrs
-      else
-        recursiveSelect (selectorIndex + 1) selectorList (builtins.getAttr selector target)
-    else
-      throw "Expected a list or an attrset";
+        # reached end of string
+        if idx == builtins.stringLength str then
+          # we only need to handle str here because other modes have a terminating character
+          if mode == "str" then
+            state.selectors
+            ++ [
+              {
+                type = "str";
+                value = state.acc_str;
+              }
+            ]
+          else
+            state.selectors
+
+        # the selector ended, so we are expected to see a .
+        else if mode == "end" then
+          if cur == "." then
+            # TODO check if stack is empty
+            recurse str (idx + 1) (
+              state
+              // {
+                stack = [ ];
+                acc_str = "";
+              }
+            )
+          else
+            throw "unexpected character ${cur} expected ."
+
+        # we are at the start of a selector
+        else if mode == "start" then
+          if cur == "*" then
+            recurse str (idx + 1) (
+              state
+              // {
+                stack = [ "end" ] ++ state.stack;
+                selectors = state.selectors ++ [ { type = "all"; } ];
+                acc_str = "";
+              }
+            )
+
+          else if cur == ''"'' then
+            recurse str (idx + 1) (
+              state
+              // {
+                stack = [
+                  "quote"
+                  "str"
+                ] ++ state.stack;
+              }
+            )
+          else if cur == "{" then
+            recurse str (idx + 1) (
+              state
+              // {
+                stack = [ "set" ] ++ state.stack;
+                acc_str = "";
+              }
+            )
+          else if cur == "\\" then
+            recurse str (idx + 1) (
+              state
+              // {
+                stack = [
+                  "escape"
+                  "str"
+                ] ++ state.stack;
+              }
+            )
+          else
+            recurse str (idx + 1) (
+              state
+              // {
+                stack = [ "str" ] ++ state.stack;
+                acc_str = "${state.acc_str}${cur}";
+              }
+            )
+
+        # inside a set multuple values {foo,bar}
+        else if mode == "set" then
+          if cur == "}" then
+            recurse str (idx + 1) (
+              state
+              // {
+                stack = [ "end" ] ++ state.stack;
+                selectors = state.selectors ++ [
+                  {
+                    type = "set";
+                    values = state.acc_selectors ++ [ state.acc_str ];
+                  }
+                ];
+                acc_str = "";
+                acc_selectors = [ ];
+              }
+            )
+          else if cur == "," then
+            recurse str (idx + 1) (
+              state
+              // {
+                acc_selectors = state.acc_selectors ++ [ state.acc_str ];
+                acc_str = "";
+              }
+            )
+          else if cur == "\\" then
+            recurse str (idx + 1) (state // { stack = [ "escape" ] ++ state.stack; })
+          else if cur == ''"'' then
+            recurse str (idx + 1) (state // { stack = [ "quote" ] ++ state.stack; })
+          else
+            recurse str (idx + 1) (state // { acc_str = "${state.acc_str}${cur}"; })
+
+        # inside a quoted string "bla"
+        else if mode == "quote" then
+          if cur == ''"'' then
+            recurse str (idx + 1) (state // { stack = builtins.tail state.stack; })
+          else if cur == "\\" then
+            recurse str (idx + 1) (state // { stack = [ "escape" ] ++ state.stack; })
+          else
+            recurse str (idx + 1) (state // { acc_str = "${state.acc_str}${cur}"; })
+
+        # we try to escape soemthing with \
+        else if mode == "escape" then
+          recurse str (idx + 1) (
+            state
+            // {
+              acc_str = "${state.acc_str}${cur}";
+              stack = builtins.tail state.stack;
+            }
+          )
+
+        # just a normal string selector
+        else if mode == "str" then
+          if cur == "." then
+            if builtins.length state.stack > 1 then
+              throw "stack unexpected length ${state.stack}"
+            else
+              recurse str (idx + 1) (
+                state
+                // {
+                  stack = [ ];
+                  acc_str = "";
+                  selectors = state.selectors ++ [
+                    {
+                      type = "str";
+                      value = state.acc_str;
+                    }
+                  ];
+                }
+              )
+          else if cur == "\\" then
+            recurse str (idx + 1) (state // { stack = [ "escape" ] ++ state.stack; })
+          else
+            recurse str (idx + 1) (state // { acc_str = "${state.acc_str}${cur}"; })
+
+        else
+          throw "unknown mode ${mode}";
+    in
+    recurse x 0 {
+      stack = [ ];
+      acc_selectors = [ ];
+      acc_str = "";
+      selectors = [ ];
+    };
 
   /**
-    parseSelector :: str -> [str]
+    applySelectors [ Selectors ] -> dict/list -> Any
 
-    # Inputs
-
-    `selector`
-
-    : 1\. Specialised attribute path string.
-
-    # Examples
-    :::{.example}
-
-    ```nix
-    parseSelector "foo.bar"
-    => [ "foo" "bar" ]
-    parseSelector ''someAttrset."foo.bar"''
-    => [ "someAttrset" "foo.bar" ]
-    parseSelector ''someAttrset.{"foo.bar","foo.baz"}''
-    => [ "someAttrset" "{foo.bar,foo.baz}" ]
-    ```
-  **/
-  parseSelector =
+      Example:
+        applySelectors [ { type = "all"; } { type = "str"; value = "foo"; } ] { x.foo = "bar"; }
+      => "bar
+    *
+  */
+  applySelectors =
+    selectors: obj:
     let
-      # mapAlternate :: (str -> [str]) -> (str -> [str]) -> [str] -> [str]
-      # Example:
-      #  mapAlternate (x: [ (x + 1) ]) (x: [ (x + 2) ]) [ 1 2 ] == [ 2 4 ]
-      mapAlternate = f: g: list:
-        let
-          len = builtins.length list;
-          go = idx: f: g:
-            if idx >= len then [ ] else f (builtins.elemAt list idx) ++ go (idx + 1) g f;
-        in go 0 f g;
-      # parseQuoted :: str -> [str]
-      # A selector parser that supports quoted strings.
-      parseQuoted = s:
-        mapAlternate
-          # Split the string by dots
-          (x: builtins.filter (x: x != "") (
-                map (builtins.replaceStrings [ "." ] [ "" ])
-                  (builtins.filter (x: !builtins.isList x) (builtins.split ''\.'' x))))
-          (x: [ x ])
-          # A list of strings separated by quotes
-          (builtins.filter (x: !builtins.isList x) (builtins.split ''"'' s));
-      splitByCurly = x: builtins.filter (x: !builtins.isList x) (builtins.split ''[{}]'' x);
-    in
-      selector:
-        mapAlternate 
-          parseQuoted
-          # `parseQuoted` the string inside the curly braces
-          (x: [ ("{" + builtins.concatStringsSep "" (parseQuoted x) + "}") ])
-          (splitByCurly selector);
+      recurse =
+        selectors: idx: obj:
+        # we are done
+        if builtins.length selectors == idx then
+          obj
 
-  select = selector: target: recursiveSelect 0 (parseSelector selector) target;
+        else
+          let
+            selector = builtins.elemAt selectors idx;
+            toInt =
+              str:
+              let
+                x = builtins.fromJSON str;
+              in
+              if builtins.isInt x then x else throw "cannot convert ${str} to int";
+          in
+          # lists
+          if builtins.isList obj then
+            if selector.type == "all" then
+              builtins.map (item: recurse selectors (idx + 1) item) obj
+            else if selector.type == "str" then
+              recurse selectors (idx + 1) (builtins.elemAt obj (toInt selector.value))
+            else if selector.type == "set" then
+              builtins.map (i: recurse selectors (idx + 1) (builtins.elemAt obj (toInt i))) selector.values
+            else
+              throw "unexpected type ${selector.type}"
+
+          # attrs
+          else if builtins.isAttrs obj then
+            if selector.type == "all" then
+              builtins.mapAttrs (_: v: recurse selectors (idx + 1) v) obj
+            else if selector.type == "str" then
+              recurse selectors (idx + 1) (builtins.getAttr selector.value obj)
+            else if selector.type == "set" then
+              let
+                filteredAttrs = builtins.listToAttrs (
+                  map (x: {
+                    name = x;
+                    value = builtins.getAttr x obj;
+                  }) selector.values
+                );
+              in
+              builtins.mapAttrs (_: v: recurse selectors (idx + 1) v) filteredAttrs
+            else
+              throw "unexpected type ${selector.type}"
+          else
+            throw "unexpected type ${builtins.typeOf obj}";
+    in
+    recurse selectors 0 obj;
+
+  /**
+      select :: str -> dict/list -> Any
+
+      Example:
+        select "*.x.y" { a.x.y = 123; }
+      =>
+        { a = 123; }
+    *
+  */
+  select = selector: obj: applySelectors (parseSelector selector) obj;
 }
--- tests.nix
+++ tests.nix
@@ -10,21 +10,92 @@ let
       };
       "foo.bar" = "baz";
       "foo.baz" = "bar";
+      "foo,baz" = "foo";
     };
     somelists = [
-      { name = "foo"; data.a = ":)"; }
-      { name = "bar"; data.b = ":|"; }
-      { name = "baz"; data.c = ":("; }
+      {
+        name = "foo";
+        data.a = ":)";
+      }
+      {
+        name = "bar";
+        data.b = ":|";
+      }
+      {
+        name = "baz";
+        data.c = ":(";
+      }
     ];
   };
 in
-  {
-    listSingle = assert ((select "somelists.0.name" testdata) == "foo"); true;
-    listAll = assert ((select "somelists.*.name" testdata) == [ "foo" "bar" "baz" ]); true;
-    listMulti = assert ((select "somelists.{0,2}.name" testdata) == [ "foo" "baz" ]); true;
-    dictSingle = assert ((select "somedict.hello" testdata) == "world"); true;
-    dictQuoted = assert ((select ''somedict."foo.bar"'' testdata) == "baz"); true;
-    dictMulti = assert ((select "somedict.foo.something.{x,y}" testdata) == { x = "hi"; y = "there";}); true;
-    dictAll = assert ((select "somedict.foo.something.*" testdata) == { x = "hi"; y = "there"; z = "sup";}); true;
-    multiQuote = assert ((select ''somedict.{"foo.bar","foo.baz"}'' testdata) == { "foo.bar" = "baz"; "foo.baz" = "bar"; }); true;
-  }
+{
+  listSingle =
+    assert ((select "somelists.0.name" testdata) == "foo");
+    true;
+  listAll =
+    assert (
+      (select "somelists.*.name" testdata) == [
+        "foo"
+        "bar"
+        "baz"
+      ]
+    );
+    true;
+  listMulti =
+    assert (
+      (select "somelists.{0,2}.name" testdata) == [
+        "foo"
+        "baz"
+      ]
+    );
+    true;
+  dictSingle =
+    assert ((select "somedict.hello" testdata) == "world");
+    true;
+  dictQuoted =
+    assert ((select ''somedict."foo.bar"'' testdata) == "baz");
+    true;
+  dictMulti =
+    assert (
+      (select "somedict.foo.something.{x,y}" testdata) == {
+        x = "hi";
+        y = "there";
+      }
+    );
+    true;
+  dictAll =
+    assert (
+      (select "somedict.foo.something.*" testdata) == {
+        x = "hi";
+        y = "there";
+        z = "sup";
+      }
+    );
+    true;
+  multiQuote =
+    assert (
+      (select ''somedict.{"foo.bar","foo.baz"}'' testdata) == {
+        "foo.bar" = "baz";
+        "foo.baz" = "bar";
+      }
+    );
+    true;
+  multiQuoteWithComma =
+    assert (
+      (select ''somedict.{"foo.bar",foo.baz,"foo,baz"}'' testdata) == {
+        "foo.bar" = "baz";
+        "foo.baz" = "bar";
+        "foo,baz" = "foo";
+      }
+    );
+    true;
+  multiEscape =
+    assert (
+      (select ''somedict.{foo\.bar,foo.baz,foo\,baz}'' testdata) == {
+        "foo.bar" = "baz";
+        "foo.baz" = "bar";
+        "foo,baz" = "foo";
+      }
+    );
+    true;
+}
